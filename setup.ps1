037c455137bd12bc35679cbad6aa1d6fe30d6c5a
398103481wdawd <mail@cruizernw.com.tr>
2025-05-01 16:34:53 +0300

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -8,33 +8,17 @@ try {
     exit 1
 }
 
-$reemoUrl = "https://download.reemo.io/reemo.setup.x64.exe"
-$installerPath = "$env:TEMP\reemo.setup.x64.exe"
+$zipUrl = "https://github.com/398103481wdawd/ReemoRDP/releases/download/re/Reemo.zip"
+$zipPath = "$env:TEMP\file.zip"
+$extractPath = "C:\Program Files\Reemo"
 
-Write-Host "[*] Downloading Reemo..."
-Invoke-WebRequest -Uri $reemoUrl -OutFile $installerPath
+mkdir "C:\Program Files\Reemo"
 
-Write-Host "[*] Installing Reemo silently..."
-Start-Process -FilePath $installerPath -ArgumentList "/S" -Wait
+Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
 
-Start-Sleep -Seconds 10
-
-# Path to Reemo config file
-$reemoConfigPath = "C:\Program Files\Reemo\service\reemo.ini"
-
-# URL to your custom reemo.ini file
-$customIniUrl = "https://github.com/398103481wdawd/ReemoRDP/raw/refs/heads/main/reemo.ini" 
-$customIniPath = "C:\Program Files\Reemo\reemo.ini"
-
-Write-Host "[*] Downloading custom reemo.ini..."
-Invoke-WebRequest -Uri $customIniUrl -OutFile $customIniPath
-
-if (Test-Path $reemoConfigPath) {
-    Copy-Item -Path $customIniPath -Destination $reemoConfigPath -Force
-    Write-Host "[+] Reemo.ini replaced successfully."
-} else {
-    Write-Warning "[-] Reemo.ini not found. Reemo may not have installed correctly."
+if (!(Test-Path -Path $extractPath)) {
+    New-Item -ItemType Directory -Path $extractPath | Out-Null
 }
 
-Write-Host "[✓] Script complete."
+Expand-Archive -Path $zipPath -DestinationPath $extractPath -Force
 
